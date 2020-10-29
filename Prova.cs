18b3fabdb0f317598734527ffc22e1736a9402bc
Joana <kaspareitjoanaalice@gmail.com>
2020-10-28 21:04:22 -0300

    criando prova1

--- Prova.cs
+++ Prova.cs
@@ -6,16 +6,23 @@ namespace Prova
 {
     public class Prova
     {
+        public string Prova1()
+        // 1- Solicite ao usuário que informe quantas horas e minutos ele trabalha por dia e qual o valor/hora de seu serviço. Imprima quanto dinheiro ele ganha ao longo de um dia trabalhado. A mensagem impressa ao usuário deve ter, no máximo, duas casas após a vírgula.
+        {
+            const int minutosEmHoras = 60;
+            const int horasNumDia
+        }
         public string Prova2(List<string> letras)
+        //2- Solicite ao usuário que informe uma palavra qualquer, porém ele deve digitar uma letra por vez. Quando o usuário informar a palavra “Finalizar”, a aplicação deve apresentar a palavra inteira.
         {
             var palavra = new StringBuilder();
-                        
+
             foreach (var item in letras)
             {
                 palavra.Append(item);
             }
 
-            
+
             return palavra.ToString();
 
             // return String.Join("", letras);
--- ProvaTest.cs
+++ ProvaTest.cs
@@ -6,6 +6,17 @@ namespace Prova
     public class ProvaTest
     {
         [Fact]
+        public void deve_retornar_o_total_de_dinheiro_que_o_usuario_ganha_num_dia_trabalhando()
+        {
+            var letras = new List<string>{"c", "e"};
+            letras.Add("l");
+
+            var prova = new Prova();
+            var palavra = prova.Prova1(letras);
+
+            Assert.Equal("cel", palavra);
+        }
+        [Fact]
         public void deve_retornar_cel_quando_informado_c_e_l()
         {
             var letras = new List<string>{"c", "e"};
@@ -16,5 +27,6 @@ namespace Prova
 
             Assert.Equal("cel", palavra);
         }
+
     }
 }
